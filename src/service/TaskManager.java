20f07e9f15d1efb060c142041fb3a4ed510e0ac3
P1r0G <pirogov27@bk.ru>
2024-06-23 16:08:24 +0300

    Исправлено удаление эпиков из History Manager.

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -9,23 +9,42 @@ import java.util.List;
 
 public interface TaskManager {
     ArrayList<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTask(int id);
+
     Task createTask(Task task);
+
     void updateTask(Task task);
+
     void delete(int id);
+
     ArrayList<Epic> getAllEpic();
+
     void deleteAllEpic();
+
     Epic getEpic(int id);
+
     Epic createEpic(Epic epic);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
+
     List<SubTask> getSubTasksInEpic(Epic epic);
+
     ArrayList<SubTask> getAllSubTask();
+
     void deleteAllSubTask();
+
     SubTask getSubTask(int id);
+
     SubTask createSubTask(SubTask subTask);
+
     void updateSubTask(SubTask subTask);
+
     void deleteSubTask(int id);
+
     List<Task> getHistoryManager();
 }
