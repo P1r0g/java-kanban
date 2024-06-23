16390da76adf2b2ba49c2f2c010d323d8421f050
P1r0G <pirogov27@bk.ru>
2024-06-23 16:04:19 +0300

    Исправлено удаление эпиков из History Manager.

--- src/service/TaskManager.java
+++ src/service/TaskManager.java
@@ -1,48 +1,31 @@
 package service;
+
 import model.Epic;
 import model.SubTask;
 import model.Task;
+
 import java.util.ArrayList;
 import java.util.List;
+
 public interface TaskManager {
-
-    ArrayList<Task> getAllTasks() ;
-
-    void deleteAllTasks() ;
-
-    Task getTask(int id) ;
-
-    Task createTask(Task task) ;
-
-    void updateTask(Task task) ;
-
-    void delete(int id) ;
-
-    ArrayList<Epic> getAllEpic() ;
-
-    void deleteAllEpic() ;
-
-    Epic getEpic(int id) ;
-
-    Epic createEpic(Epic epic) ;
-
-    void updateEpic(Epic epic) ;
-
-    void deleteEpic(int id) ;
-
-    List<SubTask> getSubTasksInEpic(Epic epic) ;
-
-    ArrayList<SubTask> getAllSubTask() ;
-
-    void deleteAllSubTask() ;
-
-    SubTask getSubTask(int id) ;
-
-    SubTask createSubTask(SubTask subTask) ;
-
-    void updateSubTask(SubTask subTask) ;
-
-    void deleteSubTask(int id) ;
-
-    List<Task> getHistoryManager() ;
+    ArrayList<Task> getAllTasks();
+    void deleteAllTasks();
+    Task getTask(int id);
+    Task createTask(Task task);
+    void updateTask(Task task);
+    void delete(int id);
+    ArrayList<Epic> getAllEpic();
+    void deleteAllEpic();
+    Epic getEpic(int id);
+    Epic createEpic(Epic epic);
+    void updateEpic(Epic epic);
+    void deleteEpic(int id);
+    List<SubTask> getSubTasksInEpic(Epic epic);
+    ArrayList<SubTask> getAllSubTask();
+    void deleteAllSubTask();
+    SubTask getSubTask(int id);
+    SubTask createSubTask(SubTask subTask);
+    void updateSubTask(SubTask subTask);
+    void deleteSubTask(int id);
+    List<Task> getHistoryManager();
 }
